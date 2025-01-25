433a8b2805ca0de999efd4d4249336999c309840
answer35 <rbanswer35@gmail.com>
2025-01-25 12:59:13 +0100

    instantnoodlep: Initialize RisingOS 6.0

--- lineage_instantnoodlep.mk
+++ lineage_instantnoodlep.mk
@@ -15,6 +15,14 @@ $(call inherit-product, device/oneplus/instantnoodlep/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# RisingOS
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="Snapdragon 865" \
+    RisingMaintainer="answer35"
+# Extras
+TARGET_HAS_UDFPS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
 PRODUCT_NAME := lineage_instantnoodlep
 PRODUCT_DEVICE := instantnoodlep
 PRODUCT_MANUFACTURER := OnePlus
